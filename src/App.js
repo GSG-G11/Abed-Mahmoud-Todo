d87f5bbee01835605f066f86953d554f280d602e
abedshamia <ab.shamia96@gmail.com>
2022-04-06 11:11:33 +0300

    Add create todo function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoListItems from './Components/TodoListItems';
 import AddTodoForm from './Components/AddTodoForm';
+import swal from 'sweetalert';
 
 const todos = [
   {
@@ -22,11 +23,28 @@ class App extends Component {
     todos,
     todoValue: '',
   };
+
+  addTodo = e => {
+    e.preventDefault();
+    const value = e.target.elements[0].value;
+    if (!value.trim()) {
+      swal('Oops...', 'You must enter a value!', 'error');
+      return;
+    }
+    const newTodo = {
+      id: this.state.todos.length + 1,
+      text: value,
+    };
+
+    this.setState({
+      todos: [...this.state.todos, newTodo],
+    });
+  };
   render() {
     const {todos, todoValue} = this.state;
     return (
       <div className="container">
-        <AddTodoForm todoValue={todoValue} />
+        <AddTodoForm todoValue={todoValue} addTodo={this.addTodo} />
         <TodoListItems todos={todos} />
       </div>
     );
